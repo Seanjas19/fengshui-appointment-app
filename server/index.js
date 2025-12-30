fb5c951cf6fcacc640ed341a6e5a4bc81569b660
Seanjas19 <seanhong19@gmail.com>
2025-12-30 23:11:53 +0800

    Modified: index.js, package,json. Added: authorization.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const db = require("./db");
+const jwt = require("jsonwebtoken");
+const authorize = require("./middeware/authorization.js");
 require("dotenv").config();
 
 const app = express();
@@ -13,6 +15,141 @@ const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
     res.send("Feng Shui Server is Running");
 });
+
+app.get("/api/appointment", authorize, async (req, res) => {
+    try{
+        const get_appointments = await db.query("SELECT * FROM appointments WHERE user_id = $1", [req.user]);
+
+        res.status(200).json({
+            message: "Appointment Retrieved Successfully!",
+            appoinments: get_appointments.rows
+        });
+    }
+    catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server error");
+    }
+});
+
+app.post("/api/login", async (req, res) => {
+    try {
+        const {user_email, user_password} = req.body;
+
+        const checkUser = await db.query("SELECT * FROM users WHERE user_email = $1", [user_email]);
+
+        if (checkUser.rows.length === 0) {
+            return res.status(401).json({message: "Invalid Email and Password!"});
+        }
+
+        const valid_password = await bcrypt.compare(
+            user_password,
+            checkUser.rows[0].user_password
+        );
+
+        if (!valid_password) {
+            return res.status(401).json({message: "Invalid Email and Password!"});
+        }
+
+        const token = jwt.sign(
+            {id: checkUser.rows[0].user_id},
+            process.env.JWT_SECRET,
+            {expiresIn: "3h"}
+        );
+
+        res.status(200).json({
+            message: "Login Successful!",
+            token: token,
+            user: {
+                id: checkUser.rows[0].user_id,
+                name: checkUser.rows[0].username
+            }
+        })
+    }
+    catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+
+app.post("/api/sign-up", async (req, res) => {
+    try {
+        const {username, user_email, user_password, user_phone} = req.body;
+
+        const existUser = await db.query("SELECT * FROM users WHERE user_email = $1", [user_email]);
+
+        if (existUser.rows.length != 0) {
+            return res.status(401).send("User already existed!");
+        }
+
+        const numSalt = 10;
+        const salt = await bcrypt.genSalt(numSalt);
+        const bcrypt_password = await bcrypt.hash(user_password, salt);
+
+        const newUser = await db.query(
+            "INSERT INTO users( username, user_email, user_password, user_phone) VALUES ($1, $2, $3, $4) RETURNING *",
+            [username, user_email, bcrypt_password, user_phone]
+        );
+
+        res.status(201).json({
+            message: "Sign Up Successful!",
+            user_id: newUser.rows[0].user_id
+        });
+    }
+    catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+app.post("/api/contact", async (req, res) => {
+    try{
+        //Extract Contact data from Request Body
+        const {contact_name, contact_email, contact_phone, contact_message} = req.body;
+
+        //Insert Contact data to Database. Why contacts(...) because if don't put it will assume contact_id as $1 and all the data for insertion will crash and messy
+        const newContact = await db.query(
+            "INSERT INTO contacts(contact_name, contact_email, contact_phone, contact_message) VALUES ($1, $2, $3, $4) RETURNING *",
+            [contact_name, contact_email, contact_phone, contact_message]
+        );
+        
+        //pass successful message to browser 
+        res.status(201).json({
+            message: "Message sent successfully!",
+            data: newContact.rows[0]
+        });
+
+    }
+    //if error it will response status as "Internal Server Error"
+    catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server error");
+    }
+
+});
+
+app.post("/api/appointment", authorize, async (req, res) => {
+
+    try{
+
+        const {appointment_date, appointment_status} = req.body;
+
+        const newAppointment = await db.query(
+            "INSERT INTO appointments(user_id, appointment_date, appointment_status) VALUES ($1, $2, $3) RETURNING *",
+            [req.user, appointment_date, appointment_status]
+        );
+        
+        res.status(201).json({
+            message: "Appointment Successful",
+            appointment: newAppointment.rows[0]
+        })
+
+    }
+    catch (err) {
+        console.error(err.message);
+        return res.status(500).send("Server Error");
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server is Running on port ${PORT}`);
---
+++ server/middleware/authorization.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+module.export = async (req, res, next) => {
+    
+    try{
+
+        const jwt_token = req.header("token");
+
+        if (!jwt_token) {
+            return res.status(403).json({message: "Unauthorized!"});
+        }
+
+        const payload = jwt.verify(jwt_token, process.env.JWT_SECRET);
+
+        req.user = payload.id;
+        
+        next();
+    }
+    catch (err) {
+        console.error(err.message);
+        return res.status(403).json({message: "Token is invalid!"});
+    }
+}

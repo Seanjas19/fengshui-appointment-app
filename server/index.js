9c3490c28453bc3d242d4b44037d2e5089c8ce50
Seanjas19 <seanhong19@gmail.com>
2026-01-20 17:58:44 +0800

    delete: remove old routes and logic code from index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const db = require("./db");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
 const authorize = require("./middleware/authorization.js");
 require("dotenv").config();
 
@@ -37,78 +35,7 @@ app.get("/api/appointment", authorize, async (req, res) => {
     }
 });
 
-//User login with authentication and authorization(JWT)
-app.post("/api/login", async (req, res) => {
-    try {
-        const {user_email, user_password} = req.body;
 
-        const checkUser = await db.query("SELECT * FROM users WHERE user_email = $1", [user_email]);
-
-        if (checkUser.rows.length === 0) {
-            return res.status(401).json({message: "Invalid Email and Password!"});
-        }
-
-        const valid_password = await bcrypt.compare(
-            user_password,
-            checkUser.rows[0].user_password
-        );
-
-        if (!valid_password) {
-            return res.status(401).json({message: "Invalid Email and Password!"});
-        }
-
-        //Give user a Wristband for Bounder to check without this is unauthorized
-        const token = jwt.sign(
-            {id: checkUser.rows[0].user_id},
-            process.env.JWT_SECRET,
-            {expiresIn: "3h"}
-        );
-
-        res.status(200).json({
-            message: "Login Successful!",
-            token: token,
-            user: {
-                id: checkUser.rows[0].user_id,
-                name: checkUser.rows[0].username
-            }
-        })
-    }
-    catch (err) {
-        console.error(err.message);
-        res.status(500).send("Server Error");
-    }
-});
-
-//Let user to sign up an account and the password they created should be hashed
-app.post("/api/sign-up", async (req, res) => {
-    try {
-        const {username, user_email, user_password, user_contact} = req.body;
-
-        const existUser = await db.query("SELECT * FROM users WHERE user_email = $1", [user_email]);
-
-        if (existUser.rows.length != 0) {
-            return res.status(401).send("User already existed!");
-        }
-
-        const numSalt = 10;
-        const salt = await bcrypt.genSalt(numSalt);
-        const bcrypt_password = await bcrypt.hash(user_password, salt);
-
-        const newUser = await db.query(
-            "INSERT INTO users( username, user_email, user_password, user_contact) VALUES ($1, $2, $3, $4) RETURNING *",
-            [username, user_email, bcrypt_password, user_contact]
-        );
-
-        res.status(201).json({
-            message: "Sign Up Successful!",
-            user_id: newUser.rows[0].user_id
-        });
-    }
-    catch (err) {
-        console.error(err.message);
-        res.status(500).send("Server Error");
-    }
-});
 
 
 //Let non-member user to create contact
